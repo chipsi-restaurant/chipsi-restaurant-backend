8d4b2f189f1be77976bd9108895f4dc2e2c5c1ed
sinusxd <senya.kolyshkin.gg@gmail.com>
2025-02-18 16:22:25 +0300

    CHIPSI-12 init commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
+	// to see how GoLand suggests fixing it.
+	s := "gopher"
+	fmt.Println("Hello and welcome, %s!", s)
+}
