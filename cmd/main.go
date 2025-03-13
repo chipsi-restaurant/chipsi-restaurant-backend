a91b87a21bd1727b6e4da165219966910203e5d4
sinusxd <senya.kolyshkin.gg@gmail.com>
2025-03-13 10:22:36 +0300

    CHIPSI-23 Поднял http-сервер

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "chipsiBackend/bootstrap"
+import (
+	"chipsiBackend/api/route"
+	"chipsiBackend/bootstrap"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
 
 func main() {
 	app := bootstrap.App()
 	defer app.CloseDbConnection()
+	router := route.Setup(app.Db)
+
+	app.Log.Info(fmt.Sprintf("Server is listening on PORT: %d", app.Cfg.Server.Port))
+	addr := ":" + strconv.Itoa(app.Cfg.Server.Port)
+	err := http.ListenAndServe(addr, router)
+	if err != nil {
+		app.Log.Error("Error starting Server: ", "err", err)
+		os.Exit(1)
+	}
 }
