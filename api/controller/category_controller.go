cdf3967e4ecf96507b08bf29fbd70ce6d6ef7624
sinusxd <senya.kolyshkin.gg@gmail.com>
2025-03-29 11:38:13 +0300

    CHIPSI-29 Добавил crud для категорий

---
+++ api/controller/category_controller.go
@@ -0,0 +1,84 @@
+package controller
+
+import (
+	"chipsiBackend/domain"
+	"encoding/json"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"strconv"
+)
+
+type CategoryController struct {
+	CategoryUsecase domain.CategoryUsecase
+}
+
+func (cc *CategoryController) Create(w http.ResponseWriter, r *http.Request) {
+	var category domain.Category
+	if err := json.NewDecoder(r.Body).Decode(&category); err != nil {
+		http.Error(w, `{"error": "`+err.Error()+`"}`, http.StatusBadRequest)
+		return
+	}
+
+	_, err := cc.CategoryUsecase.Create(r.Context(), &category)
+
+	if err != nil {
+		http.Error(w, `{"error": "`+err.Error()+`"}`, http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(category)
+
+	if err != nil {
+		http.Error(w, `{"error": "`+err.Error()+`"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cc *CategoryController) GetByID(w http.ResponseWriter, r *http.Request) {
+	idStr := chi.URLParam(r, "id")
+	if idStr == "" {
+		http.Error(w, `{"error": "id must be not null"}`, http.StatusBadRequest)
+		return
+	}
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		http.Error(w, `{"error": "id must be number"}`, http.StatusBadRequest)
+		return
+	}
+
+	category, err := cc.CategoryUsecase.GetByID(r.Context(), id)
+
+	if err != nil {
+		http.Error(w, `{"error": "not found"}`, http.StatusNotFound)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(*category); err != nil {
+		http.Error(w, `{"error": "can't encode category to json'"}`, http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cc *CategoryController) GetAll(w http.ResponseWriter, r *http.Request) {
+	include := r.URL.Query().Get("includeMenuItems")
+	if include == "" || (include != "true" && include != "false") {
+		http.Error(w, `{"error": "invalid query param"}`, http.StatusBadRequest)
+		return
+	}
+
+	categories, err := cc.CategoryUsecase.GetAll(r.Context(), include == "true")
+
+	if err != nil || len(categories) == 0 {
+		http.Error(w, `{"error": "not found"}`, http.StatusNotFound)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(categories); err != nil {
+		http.Error(w, `{"error": "can't encode category to json'"}`, http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
--- domain/category.go
+++ domain/category.go
@@ -1,7 +1,23 @@
 package domain
 
+import "context"
+
 type Category struct {
-	ID        uint   `gorm:"primaryKey"`
-	Name      string `gorm:"size:100;not null"`
-	MenuItems []MenuItem
+	ID        uint        `gorm:"primaryKey" json:"id"`
+	Name      string      `gorm:"size:100;not null" json:"name"`
+	MenuItems *[]MenuItem `json:"menuItems,omitempty"`
+}
+
+type CategoryRepository interface {
+	Create(ctx context.Context, category *Category) (*Category, error)
+	GetByID(ctx context.Context, id int64) (*Category, error)
+	GetAll(ctx context.Context, includeMenuItems bool) ([]*Category, error)
+	Delete(ctx context.Context, id int64) error
+}
+
+type CategoryUsecase interface {
+	Create(ctx context.Context, category *Category) (*Category, error)
+	GetByID(ctx context.Context, id int64) (*Category, error)
+	GetAll(ctx context.Context, includeMenuItems bool) ([]*Category, error)
+	Delete(ctx context.Context, id int64) error
 }
